27aee90f9bae74aba623ef0145163d351938f5d7
Gabriel Saenz <gsaenz1@aclaratech.com>
2024-03-16 15:48:59 -0500

    outputs format

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,6 @@ output "cr_admin_username" {
 }
 
 output "cr_admin_password" {
-  value = azurerm_container_registry.containerregistry.admin_password
+  value     = azurerm_container_registry.containerregistry.admin_password
   sensitive = true
 }
