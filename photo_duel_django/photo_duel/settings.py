cf51bbea0fb09dde97cffe04282d76866e3226d4
Gabriel Saenz <gsaenz1@aclaratech.com>
2024-03-17 19:58:23 -0500

    update allowed_hosts to *

--- photo_duel_django/photo_duel/settings.py
+++ photo_duel_django/photo_duel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['photo-duel.azurewebsites.net', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
